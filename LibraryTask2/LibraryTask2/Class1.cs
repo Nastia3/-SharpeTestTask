0132fab804e2f6221a1fb1e46f100c330f896d02
Nastia3 <linka0393@gmail.com>
2021-01-01 06:48:45 +0200

    add task2

---
+++ LibraryTask2/LibraryTask2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTask2
+{
+    [TestFixture]
+    public class Class1
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void beforeTest()
+        {
+            driver = new ChromeDriver("C:\\webDriver");
+            driver.Url = "https://www.seleniumeasy.com/";
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void afterTest()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void test1()
+        {
+            IWebElement searchField = driver.FindElement(By.Id("edit-search-block-form--2"));
+            searchField.SendKeys("Maven");
+            searchField.SendKeys(Keys.Enter);
+
+            Assert.AreEqual("https://www.seleniumeasy.com/search/node/Maven", driver.Url);
+        }
+
+    }
+}
